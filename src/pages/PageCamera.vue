632b06fdafe7379340e61e0442da86b6482880d1
KoppePan0709 <yuichi.masutani@ibm.com>
2021-08-05 01:38:09 +0900

    camera page

--- src/pages/PageCamera.vue
+++ src/pages/PageCamera.vue
@@ -1,6 +1,52 @@
 <template>
-  <q-page class="flex flex-center">
-    <h5>Home Camera</h5>
+  <q-page class="constrain-more q-pa-md">
+    <div class="camera-frame q-pa-md">
+      <img 
+        class="full-width"
+        src="https://cdn.quasar.dev/img/parallax2.jpg">
+
+    </div>
+    <div class="text-center q-pa-md">
+      <q-btn
+        round
+        color="grey-10"
+        icon="eva-camera"
+        size="lg"
+      />
+      <div class="row justify-center q-ma-md">
+        <q-input
+          v-model="text"
+          class="col col-sm-6"
+          label="Caption"
+          dense
+        />
+      </div>
+      <div class="row justify-center q-ma-md">
+        <q-input
+          v-model="text"
+          class="col col-sm-6"
+          label="Location"
+          dense
+        >
+          <template v-slot:append>
+            <q-btn
+              round
+              dense
+              flat
+              icon="eva-navigation-2-outline"
+            />
+          </template>
+        </q-input>
+      </div>
+      <div class="row justify-center q-mt-lg">
+         <q-btn
+           unelevated
+           rounded
+           color="primary"
+           label="Post Image"
+         />
+      </div>
+    </div>
   </q-page>
 </template>
 
@@ -9,3 +55,11 @@ export default {
   name: 'PageCamera'
 }
 </script>
+
+<style lang="sass">
+  .camera-frame
+    border: 2px solid $grey-10
+    border-radius: 10px
+
+
+</style>
